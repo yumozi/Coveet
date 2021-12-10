4893364a927ddfc20e92b86dbe03a1a2bd431ed5
JeremyXie123 <jeremy.xie@tdsb.on.ca>
2021-12-10 02:03:27 -0500

    Add documentation to the NAChoropleth class

--- NAChoropleth.py
+++ NAChoropleth.py
@@ -8,61 +8,69 @@ from PyQt5 import QtWidgets, QtWebEngineWidgets
 
 
 class NAChoropleth(QtWidgets.QMainWindow):
+    """A class meant to aid in the creation and management of an interactive map, with additional
+    choropleth functionality and ability to visualize filtered data.
+
+    TODO: specify instance attributes?
+    """
     def __init__(self):
         super().__init__()
-        self.america_covid_data = pd.read_csv("US_Unemployment_Oct2012.csv")
-        self.canada_covid_data = pd.read_csv("canada_test.csv")
+        # Load data from files
+        self._american_covid_data = pd.read_csv("US_Unemployment_Oct2012.csv")
+        self._canadian_covid_data = pd.read_csv("canada_test.csv")
 
         # Window initialization
         self.setWindowTitle(self.tr("MAP PROJECT"))
         self.setFixedSize(1200, 1000)
 
         # GUI initalization
-        self.view = QtWebEngineWidgets.QWebEngineView()
-        self.view.setContentsMargins(25, 25, 25, 25)
+        self._view = QtWebEngineWidgets.QWebEngineView()
+        self._view.setContentsMargins(25, 25, 25, 25)
 
         base_frame = QtWidgets.QWidget()
         self.setCentralWidget(base_frame)
         h_layout = QtWidgets.QHBoxLayout(base_frame)
 
-        self.combo = QtWidgets.QComboBox()
+        self._combo = QtWidgets.QComboBox()
+
+        # Add arbitrary maximums for property sliders
+        self._maxes = {"Covid Cases": 10000, "Population": 100000}
 
         # Combobox
-        self.maxes = {"Covid Cases": 10000, "Population": 100000}
-        for x in self.maxes:
-            self.combo.addItem(x)
-        self.combo.activated[str].connect(self.combo_changed)
+        for x in self._maxes:
+            self._combo.addItem(x)
+        self._combo.activated[str].connect(self.combo_changed)
 
         # Slide value display lable
-        self.slide_val = QtWidgets.QLabel('0')
-        self.slide_val.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+        self._slide_val = QtWidgets.QLabel('0')
+        self._slide_val.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
 
         # Slider
-        self.slider = QtWidgets.QSlider(Qt.Vertical)
-        self.slider.setFocusPolicy(Qt.StrongFocus)
-        self.slider.setTickInterval(10)
-        self.slider.setSingleStep(1)
-        self.slider.valueChanged[int].connect(self.slide_changed)
-        self.slider.setMaximum(self.maxes["Covid Cases"])
+        self._slider = QtWidgets.QSlider(Qt.Vertical)
+        self._slider.setFocusPolicy(Qt.StrongFocus)
+        self._slider.setTickInterval(10)
+        self._slider.setSingleStep(1)
+        self._slider.valueChanged[int].connect(self.slide_changed)
+        self._slider.setMaximum(self._maxes["Covid Cases"])
 
         adjust_frame = QtWidgets.QWidget()
         v_layout = QtWidgets.QVBoxLayout(adjust_frame)
 
-        v_layout.addWidget(self.combo)
-        v_layout.addWidget(self.slide_val)
-        v_layout.addWidget(self.slider)
+        v_layout.addWidget(self._combo)
+        v_layout.addWidget(self._slide_val)
+        v_layout.addWidget(self._slider)
 
         h_layout.addWidget(adjust_frame)
-        h_layout.addWidget(self.view, stretch=1)
+        h_layout.addWidget(self._view, stretch=1)
 
         # Map widget creation
-        self.map = folium.Map(location=[40, -95], zoom_start=4, tiles="Stamen Toner")
+        self._map = folium.Map(location=[40, -95], zoom_start=4, tiles="Stamen Terrain")
 
         # Choropleth configuration
-        self.america_choropleth = folium.Choropleth(
+        self._america_choropleth = folium.Choropleth(
             geo_data="us-states.json",
             name="America",
-            data=self.america_covid_data,
+            data=self._american_covid_data,
             columns=["State", "Unemployment"],
             key_on="feature.id",
             fill_color="YlGn",
@@ -71,12 +79,12 @@ class NAChoropleth(QtWidgets.QMainWindow):
             legend_name="America",
             highlight=True,
         )
-        self.america_choropleth.add_to(self.map)
+        self._america_choropleth.add_to(self._map)
 
-        self.canada_choropleth = folium.Choropleth(
+        self._canada_choropleth = folium.Choropleth(
             geo_data="canada_provinces.geojson",
             name="Canada",
-            data=self.canada_covid_data,
+            data=self._canadian_covid_data,
             columns=["Province", "Value"],
             key_on="feature.properties.name",
             fill_color="YlGn",
@@ -85,28 +93,28 @@ class NAChoropleth(QtWidgets.QMainWindow):
             legend_name="Canada",
             highlight=True,
         )
-        self.canada_choropleth.add_to(self.map)
+        self._canada_choropleth.add_to(self._map)
 
-        folium.LayerControl().add_to(self.map)  # add layer control and toggling to the map
-
-        #folium.TileLayer('cartodbdark_matter', overlay=True, name="dark mode").add_to(self.map)
-        #folium.TileLayer('cartodbpositron', overlay=True, name="light mode").add_to(self.map)
+        folium.LayerControl().add_to(self._map)  # add layer control and toggling to the map
 
         # Connect the map and api
         data = io.BytesIO()
-        self.map.save(data, close_file=False)
-        self.view.setHtml(data.getvalue().decode())
+        self._map.save(data, close_file=False)
+        self._view.setHtml(data.getvalue().decode())
 
-    def slide_changed(self, value):
-        self.slide_val.setText(str(value))
+    def slide_changed(self, value: int) -> None:
+        """Callback to update a lable to show the slider's current value"""
+        self._slide_val.setText(str(value))
 
-    def combo_changed(self, text):
-        self.slider.setMaximum(self.maxes[text])
+    def combo_changed(self, text: str) -> None:
+        """Callback that updates the map when a new selection is made to the combo box"""
+        # TODO: Find a way to update the map dynamically
+        self._slider.setMaximum(self._maxes[text])
         print(text)
-        self.canada_choropleth = folium.Choropleth(
+        self._canada_choropleth = folium.Choropleth(
             geo_data="canada_provinces.geojson",
             name="Canada",
-            data=self.canada_covid_data,
+            data=self._canadian_covid_data,
             columns=["Province", "Value"],
             key_on="feature.properties.name",
             fill_color="OrRd",
@@ -115,11 +123,11 @@ class NAChoropleth(QtWidgets.QMainWindow):
             legend_name="Canada",
             highlight=True,
         )
-        self.canada_choropleth.add_to(self.map)
-        folium.LayerControl().add_to(self.map)
+        self._canada_choropleth.add_to(self._map)
+        folium.LayerControl().add_to(self._map)
         data = io.BytesIO()
-        self.map.save(data, close_file=False)
-        self.view.setHtml(data.getvalue().decode())
+        self._map.save(data, close_file=False)
+        self._view.setHtml(data.getvalue().decode())
 
 if __name__ == "__main__":
     App = QtWidgets.QApplication(sys.argv)
